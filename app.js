b5bccca0d1ced72299a295e38d6195bb38c544db
Rob Neville <robertneville73@gmail.com>
2015-11-07 20:15:32 -0500

    Basic createIssue working

--- app.js
+++ app.js
@@ -1,89 +1,131 @@
 var express = require('express');
 var app = express();
+var request = require('request');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 var GitHubApi = require("github");
 
-var fd = require('freshdesk');
+var nconf = require('nconf');
 
-var repo = "https://github.com/robneville73/github-freshdesk";
-var githubUser = "robneville73";
-var fd_api = 'B1g7XInX5j12AhRc0htf';
-var fd_url = 'http://retailarchitects.freshdesk.com';
+nconf.argv().env().file({
+  file: './package.json'
+});
+
+var repo = nconf.get('repo');
+var githubUser = nconf.get('githubUser');
+var githubPassword = nconf.get('githubPassword');
+var fd_api = nconf.get('fd_api');
+var fd_url = nconf.get('fd_url');
 
 var Freshdesk = new fd(fd_url, fd_api);
 
 var github = new GitHubApi({
   version: "3.0.0",
-  debug: true
+  debug: false,
+  pathPrefix: "",
+  host: "api.github.com",
+  protocol: "https",
+  headers: {
+    "user-agent": "robneville73-github-freshdesk"
+  }
 });
-
-//just making sure it works
-// github.user.getFollowingFromUser({
-//   user: "robneville73"
-// }, function(err, res) {
-//   console.log(JSON.stringify(res));
-// });
-
-//just making sure it works
-// Freshdesk.listTickets(function(err, res, body) {
-//   console.log("This tickets are: ", body);
-// });
 
 //
 // freshdeskHooks
 //
-app.post('/api/freshdeskHook/createIssue/:id', function(req, res) {
-  // * connects to freshdesk API to get ticket details
-  // * connects to github API to create a new issue
-  // * The content of the issue is copied from the ticket data
-  // * "linked" label added to issue
-  // * A new comment is added to the issue with the URL to the ticket
-  // * The ticket is updated to reference issue # (custom field on ticket)
-  // * A new note is added to the ticket with the URL to the issue.
+app.post('/api/freshdeskHook/createIssue/:id', function(apiRequest, response) {
 
-  var ticketId = req.params.id;
+  var ticketDetails = {
+    id: apiRequest.params.id
+  };
+  var issueDetails = {};
 
-  var ticket = Freshdesk.getTicket(ticketId, function(err, res, body) {
-    //grab new ticket details
-    //https://freshdesk.com/api#ticket
-    var subject = body.subject;
-    var description = body.description;
-    var ticketUrl = fd_url + '/' + body.display_id;
-    var issueId;
+  Freshdesk.getTicket(ticketDetails.id, getTicketDetails);
 
+  function getTicketDetails(err, res, body) {
+    if(err) {
+      handleError("Error getting ticket details", err);
+    } else {
+      body = JSON.parse(body);
+
+      ticketDetails.subject = body.helpdesk_ticket.subject;
+      ticketDetails.description = body.helpdesk_ticket.description;
+      ticketDetails.ticketUrl = fd_url + '/' + body.helpdesk_ticket.display_id;
+
+      createIssue();
+    }
+  }
+
+  function createIssue() {
     //create new github issue
     //http://mikedeboer.github.io/node-github/#issues.prototype.create
+    authenticateGitHub();
     github.issues.create({
       user: githubUser,
       repo: repo,
-      title: subject,
-      body: description,
+      title: ticketDetails.subject,
+      body: ticketDetails.description,
       labels: ['linked']
-    }, function(err, result) {
-      if(!err) {
-        issueId = result.id;
+    }, function(err, result){
+      if(err) {
+        handleError("Error creating issue", err);
+      } else {
+        issueDetails.number = result.number;
+        createComment();
       }
     });
+  }
 
-    if(issueId) {
-      //add comment to issue with link to ticket
-      github.issues.createComment({
-        user: githubUser,
-        repo: repo,
-        number: issueId,
-        body: ticketUrl
-      }, null);
+  function createComment() {
+    //add comment to issue with link to ticket
+    authenticateGitHub();
+    github.issues.createComment({
+      user: githubUser,
+      repo: repo,
+      number: issueDetails.number,
+      body: ticketDetails.ticketUrl
+    }, function(err, results) {
+      if(err) {
+        handleError("Error creating comment", err);
+      } else {
+        updateTicket();
+      }
+    });
+  }
 
-      //update ticket custom field with issue number
-      Freshdesk.putTicket(ticketId, {
-        "helpdesk_ticket": {
-          "custom_field": {
-            "githubissue": issueId
-          }
+  function updateTicket() {
+    var data = {
+      "helpdesk_ticket": {
+        "custom_field": {
+          "githubissue": issueDetails.number
         }
-      }, null);
-    }
-  });
+      }
+    };
+    var url = '/helpdesk/tickets/' + ticketDetails.id + '.json';
+    Freshdesk.put(url, data, function(err, res, body) {
+      if(err) {
+        handleError("Error creating githubissue link on FreshDesk", err);
+      } else {
+        response.status(201);
+      }
+    });
+  }
+
+  function handleError(msg, err) {
+    console.log(msg + ' ', JSON.parse(err));
+    response.status(500).send(msg + ' '+ JSON.parse(err));
+  }
+
+  function authenticateGitHub() {
+    github.authenticate({
+      type: "basic",
+      username: githubUser,
+      password: githubPassword
+    });
+  }
 });
 
 app.post('/api/freshdeskHook/linkIssue/:id', function(req, res) {
@@ -122,5 +164,5 @@ var server = app.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('freshdesk-gitub app listening at http://%s:%s', host, port);
 });
