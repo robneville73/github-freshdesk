cbe6b3714dc92330954d306de563658a3601efd2
Rob Neville <robertneville73@gmail.com>
2015-11-14 10:16:06 -0500

    Move config loading into module

--- app.js
+++ app.js
@@ -2,24 +2,14 @@ var express = require('express');
 var app = express();
 var request = require('request');
 var bodyParser = require('body-parser');
+var toMarkdown = require('to-markdown');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 var GitHubApi = require("github");
 
-var nconf = require('nconf');
-
-nconf.file({
-  file: 'config.json'
-});
-
-var repo = nconf.get('repo');
-var githubUser = nconf.get('githubUser');
-var githubPassword = nconf.get('githubPassword');
-var fd_api = nconf.get('fd_api');
-var fd_url = nconf.get('fd_url');
-var fd_customfield = nconf.get('fd_customfield');
+var config_data = require('./lib/config');
 
 var github = new GitHubApi({
   version: "3.0.0",
@@ -33,12 +23,18 @@ var github = new GitHubApi({
 });
 
 function ticketUrl(id) {
-  return fd_url+'/helpdesk/tickets/'+id+'.json';
+  return config_data.fd_url+'/helpdesk/tickets/'+id+'.json';
+}
+
+function userUrl(id) {
+  return config_data.fd_url+'/contacts/'+id+'.json';
 }
 
 //
 // freshdeskHooks
 //
+
+//create a new github issue from a freshdesk ticket because the fd ticket
 app.post('/api/freshdeskHook/createIssue/:id', function(apiRequest, response) {
 
   var ticketDetails = {
@@ -52,16 +48,16 @@ app.post('/api/freshdeskHook/createIssue/:id', function(apiRequest, response) {
   // github issue in
   var realCustomFieldName;
   request({
-    url: fd_url + '/ticket_fields.json',
+    url: config_data.fd_url + '/ticket_fields.json',
     method: 'GET',
     auth: {
-      user: fd_api,
+      user: config_data.fd_api,
       pass: 'X'
     }
   }, function(err, res, body) {
     var fields = JSON.parse(body);
     for(var i=0; i < fields.length; i++) {
-      if(fields[i].ticket_field.label_in_portal === fd_customfield) {
+      if(fields[i].ticket_field.label_in_portal === config_data.fd_customfield) {
         realCustomFieldName = fields[i].ticket_field.name;
         lookupTicket();
       }
@@ -74,7 +70,7 @@ app.post('/api/freshdeskHook/createIssue/:id', function(apiRequest, response) {
       url: ticketUrl(ticketDetails.id),
       method: 'GET',
       auth: {
-        user: fd_api,
+        user: config_data.fd_api,
         pass: 'X'
       }
     }, extractTicketDetails);
@@ -89,7 +85,7 @@ app.post('/api/freshdeskHook/createIssue/:id', function(apiRequest, response) {
 
       ticketDetails.subject = body.helpdesk_ticket.subject;
       ticketDetails.description = body.helpdesk_ticket.description;
-      ticketDetails.ticketUrl = fd_url + '/' + body.helpdesk_ticket.display_id;
+      ticketDetails.ticketUrl = config_data.fd_url + '/' + body.helpdesk_ticket.display_id;
 
       createIssue();
     }
@@ -101,8 +97,8 @@ app.post('/api/freshdeskHook/createIssue/:id', function(apiRequest, response) {
     //http://mikedeboer.github.io/node-github/#issues.prototype.create
     authenticateGitHub();
     github.issues.create({
-      user: githubUser,
-      repo: repo,
+      user: config_data.githubUser,
+      repo: config_data.repo,
       title: ticketDetails.subject,
       body: ticketDetails.description,
       labels: ['linked']
@@ -121,8 +117,8 @@ app.post('/api/freshdeskHook/createIssue/:id', function(apiRequest, response) {
     //add comment to issue with link to ticket
     authenticateGitHub();
     github.issues.createComment({
-      user: githubUser,
-      repo: repo,
+      user: config_data.githubUser,
+      repo: config_data.repo,
       number: issueDetails.number,
       body: ticketDetails.ticketUrl
     }, function(err, results) {
@@ -149,7 +145,7 @@ app.post('/api/freshdeskHook/createIssue/:id', function(apiRequest, response) {
       url: ticketUrl(ticketDetails.id),
       method: 'PUT',
       auth: {
-        user: fd_api,
+        user: config_data.fd_api,
         pass: 'X'
       },
       json: true,
@@ -172,9 +168,70 @@ app.post('/api/freshdeskHook/createIssue/:id', function(apiRequest, response) {
   function authenticateGitHub() {
     github.authenticate({
       type: "basic",
-      username: githubUser,
-      password: githubPassword
+      username: config_data.githubUser,
+      password: config_data.githubPassword
     });
+  }
+});
+
+app.post('/api/freshdeskHook/createComment/:id', function(req, res) {
+  var ticketDetails = {
+    id: apiRequest.params.id
+  };
+  var issueDetails = {};
+
+  //call FreshDesk API to retrieve ticket
+  function lookupTicket() {
+    request({
+      url: ticketUrl(ticketDetails.id),
+      method: 'GET',
+      auth: {
+        user: config_data.fd_api,
+        pass: 'X'
+      }
+    }, extractTicketDetails);
+  }
+
+  //parse relevant details out of FD ticket.
+  function extractTicketDetails(err, res, body) {
+    if(err) {
+      handleError("Error getting ticket details", err);
+    } else {
+      body = JSON.parse(body);
+      var latest_note = body.helpdesk_ticket.notes[body.helpdesk_ticket.notes.length-1];
+      ticketDetails.note = toMarkdown(latest_note.body_html);
+      ticketDetails.note_user = latest_note.user_id;
+
+      lookupUserDetails();
+    }
+  }
+
+  //get name of user_id that submitted the latest note
+  function lookupUserDetails() {
+    request({
+      url: userUrl(ticketDetails.note_user),
+      method: 'GET',
+      auth: {
+        user: config_data.fd_api,
+        pass: 'X'
+      }
+    }, extractUserDetails);
+  }
+
+  function extractUserDetails(err, res, body) {
+    if(err) {
+      handleError("Error retreiving user details", err);
+    } else {
+      body = JSON.parse(body);
+      ticketDetails.note_user = body.user.name;
+
+      //nextStep();
+    }
+  }
+
+  function handleError(msg, err) {
+    console.log(msg + ' ', err);
+    response.status(500).send(msg + ' '+ err);
   }
 });
 
@@ -188,13 +245,6 @@ app.post('/api/freshdeskHook/linkIssue/:id', function(req, res) {
 app.post('/api/freshdeskHook/resolveIssue/:id', function(req, res) {
   res.json({
     message: "resolveIssue",
-    ticketId: req.params.id
-  });
-});
-
-app.post('/api/freshdeskHook/createComment/:id', function(req, res) {
-  res.json({
-    message: "createComment",
     ticketId: req.params.id
   });
 });
